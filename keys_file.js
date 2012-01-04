b01f4bc003c62a4579f0a97aaa54dc7d319264a4
Matt Kane <matt@clevr.ltd.uk>
2012-01-04 17:08:18 +0000

    Delete revoked keys as we're now public.

--- keys_file.js
+++ keys_file.js
@@ -1,3 +1,3 @@
-exports.twitterConsumerKey= "gCa1fv4rGqy02aizbxkg";
-exports.twitterConsumerSecret= "geVK7md7GwCu0Ks4q1zpYMKNNAGbO7DnYeQM2JZu8";
+exports.twitterConsumerKey= "";
+exports.twitterConsumerSecret= "";
 exports.twitterCallback="http://tweetd.lt:3000/callback";
